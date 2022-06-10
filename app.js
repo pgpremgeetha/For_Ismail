625d7c9d897bff55348f970a38013c9a2d10a97f
Prem Prakash Pandiarajan Geetha <pgpremgeetha@gmail.com>
2022-06-10 13:23:02 +0530

    added Love back

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const logo = document.querySelectorAll("#line1 path");
+const logo = document.querySelectorAll("#line2 path");
 
 for(let i = 0; i<logo.length; i++){
 console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
